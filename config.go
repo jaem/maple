de55dd3c4b1bbba24428125e8c15666a52d544c6
jave <javantan@>
2018-03-06 08:06:17 +0800

    NotSureIfThisWorks: added nim - revert if not working

--- config.go
+++ config.go
@@ -6,28 +6,27 @@ import (
 
 type config struct {
 	// Server
-	Server_Port			string
-	Server_Timeout 	time.Duration  // in secs
+	Server_Port    string
+	Server_Timeout time.Duration // in secs
 
-	Db_MaxIdle			int
-	Db_MaxConn			int
-	Db_Alias				string
-	Db_Auth					string
+	Db_MaxIdle int
+	Db_MaxConn int
+	Db_Alias   string
+	Db_Auth    string
 
 	DebugMode bool
 }
 
 func NewConfig() *config {
-	return &config {
-		Server_Port: ":3001",
+	return &config{
+		Server_Port:    ":3001",
 		Server_Timeout: 10 * time.Second,
 
 		Db_MaxIdle: 30,
 		Db_MaxConn: 30,
-		Db_Alias: "default",
-		Db_Auth: "jenesis:rakaboon88@/baboo?charset=utf8",
+		Db_Alias:   "default",
+		Db_Auth:    "jenesis:rakaboon88@/baboo?charset=utf8",
 
 		DebugMode: true,
 	}
 }
-
